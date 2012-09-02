019113806b98e166f554ef7e024821f4b37742b9
Stephen Thoma <modoatk@gmail.com>
2012-09-02 01:19:44 -0500

    Add ignoring doitlive/dropbox/*

--- test_adapter.py
+++ test_adapter.py
@@ -3,8 +3,6 @@ from doitlive.dropbox.rest import ErrorResponse
 y=dropbox_adapter.DropboxAdapter('h9j5r8xzwq8twxn','af33a3wtka2t8tc')
 y.login()
 
-try:
-	print y.read_file('/six.py')
-except ErrorResponse as e:
-	print e.status == 404
+y.list_directory_contents('/')
+
 
